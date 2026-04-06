2fe08d43b3c8efed52bb841d7e2bf1016c727bc8
Fábio do Rosarío <fovdr@iscte-iul.pt>
2026-04-06 23:12:21 +0100

    Merge remote-tracking branch 'origin/T4finalizado-e-testado-E-separei-a-Stone' into T4finalizado-e-testado-E-separei-a-Stone
    # Conflicts:
    #	src/main/scala/Tui.scala

--- src/main/scala/Tui.scala
+++ src/main/scala/Tui.scala
@@ -5,6 +5,7 @@ object Tui {
 
 
   def auxTUI(lst: List[Option[Stone.stone]]): String = lst match {
+    case Nil => ""
     case x :: xs =>
       (x match {
         case Some(Stone.Black) => "B "
@@ -14,10 +15,25 @@ object Tui {
       }) + auxTUI(xs)
   }
 
+  def mostrar(board: Board, rows: Int, cols: Int): Unit = {
+    // Cabeçalho
+    def printHeader(col: Int): Unit = col match {
+      case c if c == cols => println()
+      case c =>
+        val letra = ('A' + c).toChar
+        print(s" $letra")
+        printHeader(c + 1)
+    }
+
+    print("  ")
+    printHeader(0)
+    mostrarRec(board, rows, cols, 0)
+  }
 
   def mostrarRec(board: Board, rows: Int, cols: Int, currentRow: Int): Unit = currentRow match {
     case r if r == rows => ()
     case r =>
+      print(String.format("%2d ", r + 1))
       val linha = (0 until cols).map(y => board.get((r, y))).toList
       println(auxTUI(linha))
       mostrarRec(board, rows, cols, r + 1)
